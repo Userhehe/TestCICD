4138b42dd0f14de8913713c6d5ff6359db40ddca
pkm4949 <pkm4949@gmail.com>
2024-02-23 09:25:50 +0900

    테스트

---
+++ testspringboot/src/test/java/com/pkm/edu/TestspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkm.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
